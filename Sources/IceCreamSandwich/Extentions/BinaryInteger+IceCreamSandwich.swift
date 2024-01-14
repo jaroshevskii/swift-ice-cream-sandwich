a6a866a85ca68f803936bf10bc90cbebbf38385b
jaroshevskii <jaroshevskiisasha@gmail.com>
2024-01-14 22:26:22 +0200

    Add integers support

---
+++ Sources/IceCreamSandwich/Extentions/BinaryInteger+IceCreamSandwich.swift
@@ -0,0 +1,29 @@
+//
+//  BinaryInteger+IceCreamSandwich.swift
+//  IceCreamSandwich
+//
+
+public extension BinaryInteger {
+    /// Determines whether the integer representation follows the pattern of an Ice Cream Sandwich.
+    ///
+    /// An Ice Cream Sandwich pattern in this context involves treating the integer as a string
+    /// and checking if it adheres to the same rules as described in the `isIceCreamSandwich` property
+    /// for collections.
+    ///
+    /// Examples of Ice Cream Sandwich entities:
+    ///
+    /// ```swift
+    /// 11211
+    /// 1221
+    /// 10101
+    /// ```
+    ///
+    /// Examples not considered Ice Cream Sandwich:
+    ///
+    /// ```swift
+    /// 121 // Does not have three segments
+    /// 1231 // Does not have identical outer segments
+    /// 10011 // Does not have identical outer segments
+    /// ```
+    var isIceCreamSandwich: Bool { String(self).isIceCreamSandwich }
+}
